9ee854099b4224c5ba71248b164677d2038055ea
Hobby-stack <mbassijoel7@gmail.com>
2025-08-20 14:31:34 +0100

    Update MesTutos.vue

--- src/components/MesTutos.vue
+++ src/components/MesTutos.vue
@@ -8,7 +8,7 @@
         <h2 class="text-highlight-1 section-title">Ma formation</h2>
         <p class="description"><span class="text-highlight-2">Curieux</span> de nature, j'ai toujours vu la formation comme <span class="text-highlight-2">une aventure</span>. Chaque cours, chaque <span class="text-highlight-2">projet académique</span> a été une opportunité d'approfondir 
             ma passion pour <span class="text-highlight-2">le développement</span> et <span class="text-highlight-2">la création</span>. Ici, je vous partage les chemins que j'ai empruntés pour acquérir l'expertise qui me permet 
-            aujourd'hui de donner vie à des <span class="text-highlight-2">idées ambitieuses</span>:
+            aujourd'hui de donner vie à des <span class="text-highlight-2">idées ambitieuses</span> :
         </p>
         <br>
         <div id="formations">
@@ -113,4 +113,5 @@
     }
 
 </style>
+
 
