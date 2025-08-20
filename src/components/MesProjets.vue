44cc26d042a30e3d2ad8fb8655eca17c46593ba2
Hobby-stack <mbassijoel7@gmail.com>
2025-08-20 14:39:14 +0100

    Update MesProjets.vue

--- src/components/MesProjets.vue
+++ src/components/MesProjets.vue
@@ -3,46 +3,46 @@ import { ref } from 'vue';
 import ProjectSingle from './ProjectSingle.vue';
 let projects = ref([
     {
-        name:"Site vitrine du Groupe Scolaire Bilingue Queen-Mary",
-        image:"QM.png",
-        tags:"HTML;CSS;JS",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a.",
-        tasks:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a."
+        name: "Site vitrine du Groupe Scolaire Bilingue Queen-Mary",
+        image: "QM.png",
+        tags: "HTML;CSS;JavaScript;Responsive Design",
+        description: "Site vitrine moderne et responsive pour une institution éducative de prestige, mettant en valeur son programme bilingue et son environnement d'apprentissage exceptionnel.",
+        tasks: "Conception de l'interface utilisateur;Développement front-end responsive;Intégration d'une galerie photo dynamique;Optimisation SEO pour la visibilité locale;Implémentation d'un formulaire de contact fonctionnel"
     },
     {
-        name:"Site vitrine de KMSTrack",
-        image:"KMST.png",
-        tags:"HTML;CSS;JS",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a.",
-        tasks:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a."
+        name: "Site vitrine de KMSTrack",
+        image: "KMST.png",
+        tags: "HTML;CSS;JavaScript;UI/UX Design",
+        description: "Plateforme présentant une solution innovante de tracking et gestion de flotte automobile, mettant l'accent sur la simplicité d'utilisation et les fonctionnalités techniques.",
+        tasks: "Design d'une interface utilisateur intuitive;Création d'animations interactives pour les features;Intégration de témoignages clients;Développement de sections features détaillées;Optimisation des performances de chargement"
     },
     {
-        name:"Blog d'actualite",
-        image:"projet.png",
-        tags:"VueJS;NodeJS",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a.",
-        tasks:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a."
+        name: "Blog d'actualité",
+        image: "projet.png",
+        tags: "VueJS;NodeJS;Express;MySQL",
+        description: "Application web complète permettant la publication, l'édition et la gestion d'articles avec système d'authentification et espace administrateur.",
+        tasks: "Créeation d'une API REST avec Node.js;Développement du frontend avec Vue.js;Mise en place de l'authentification JWT;Conception de la base de données MySQL;Implémentation d'un CRUD complet pour les articles"
     },
     {
-        name:"Application de Gestion d'utilisateurs",
-        image:"projet.png",
-        tags:"Vuetify;NodeJS",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a.",
-        tasks:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a."
+        name: "Application de Gestion d'utilisateurs",
+        image: "projet.png",
+        tags: "Vuetify;NodeJS;Express;MySQL",
+        description: "Dashboard administratif permettant la gestion complète des utilisateurs avec fonctionnalités de création, modification, suppression et analyse des données.",
+        tasks: "Développement d'une interface avec Vuetify;Création d'un système de roles et permissions;Implémentation de la pagination et filtres;Conception de formulaires de validation;Sécurisation des endpoints API"
     },
     {
-        name:"Landing page application de gestion d'impression",
-        image:"imp.png",
-        tags:"VueJS",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a.",
-        tasks:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a."
+        name: "Landing page application de gestion d'impression",
+        image: "imp.png",
+        tags: "VueJS;GSAP;Responsive Design",
+        description: "Page de destination haute performance pour une application SaaS de gestion d'impression, convertissant les visiteurs en leads qualifiés.",
+        tasks: "Design d'une interface utilisateur intuitive;Création d'animations interactives pour les features;Intégration de témoignages clients;Développement de sections features détaillées;Optimisation des performances de chargement"
     },
     {
-        name:"Application de Chat en temps reel",
-        image:"projet.png",
-        tags:"VueJS;NodeJS",
-        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a.",
-        tasks:"Lorem ipsum dolor sit amet consectetur, adipisicing elit; Mollitia consequuntur eum voluptatem illum; ad est aliquam quaerat dolorem consectetur dolor a."
+        name: "Application de Chat en temps réel",
+        image: "projet.png",
+        tags: "VueJS;NodeJS;Socket.io;WebSockets",
+        description: "Application de messagerie instantanée avec fonctionnalités avancées comme les rooms, les messages privés et la notification en temps réel.",
+        tasks: "Mise en place de Socket.io pour le real-time;Interface utilisateur temps réel avec Vue.js;Système de notifications push;Gestion des connexions simultanées;Chiffrement des messages pour la sécurité"
     },
 ])
 </script>
@@ -61,4 +61,5 @@ let projects = ref([
     }
 
 </style>
+
 
